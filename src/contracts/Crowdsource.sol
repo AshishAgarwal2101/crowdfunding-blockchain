ba71c1d642300fd5968c98964c037acc0329c60b
Ashish <ashish21.01agarwal@gmail.com>
2024-04-25 18:10:10 -0700

    fund campaign and getcampaigns

--- src/contracts/Crowdsource.sol
+++ src/contracts/Crowdsource.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
 
 contract Crowdsource {
     string public constant symbol = "ETH";
@@ -9,15 +10,18 @@ contract Crowdsource {
     enum CampaignState { CREATED, FUNDRAISING, COMPLETE, INCOMPLETE }
     struct Campaign {
         uint id;
+        address owner;
         string description;
         uint targetFunds; //in gwei (1 ETH = 10^9 gwei)
         uint campaignEndTime;
+        uint currFunds;
         uint positiveVotes;
         uint negativeVotes;
         CampaignState state;
     }
 
     mapping(uint => Campaign) campaignMap;
+
 
     constructor() public {
         authorized = msg.sender;
@@ -26,20 +30,41 @@ contract Crowdsource {
 
     //duration is in days
     function createCampaign(string memory description, uint targetFunds, uint duration) public {
-        require(targetFunds > 10000000, "Minimum target must be 0.01 ETH");
+        require(targetFunds >= 10000000, "Minimum target must be 0.01 ETH");
         require(duration > 10, "Minimum duration must be 10 days");
 
         Campaign memory campaign = Campaign(
-            ++lastId, 
+            ++lastId,
+            msg.sender,
             description, 
             targetFunds, 
             block.timestamp + (duration * 24 * 60 * 60),
+            0,
             0,
             0,
             CampaignState.CREATED
         );
 
         campaignMap[lastId] = campaign;
+    }
+
+    function getCampaigns() public view returns (Campaign[] memory campaigns) {
+        Campaign[] memory campaignArr = new Campaign[](lastId);
+        for (uint256 i = 1; i <= lastId; i++) {
+            campaignArr[i - 1] = campaignMap[i];
+        }
+        return campaignArr;
+    }
+
+    function fundCampaign(uint campaignId) public payable {
+        require(msg.value > 0, "No ether was sent as part of transfer");
+        require(campaignMap[campaignId].id == campaignId, "Invalid campaign ID");
+        require(campaignMap[campaignId].state == CampaignState.FUNDRAISING);
+
+        campaignMap[campaignId].currFunds += msg.value;
+        if(campaignMap[campaignId].currFunds >= campaignMap[campaignId].targetFunds) {
+            campaignMap[campaignId].state == CampaignState.COMPLETE;
+        }
     }
 
     function getRandomString() public pure returns(string memory) {
